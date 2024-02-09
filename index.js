83250bf23a0dd6804440883058483c978cabc99f
crystalyn <00crystalynthompson@gmail.com>
2024-02-09 08:46:25 -0700

    it doesn't work :'( at lest the js page is connected

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hi");
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+//const ctx2 = canvas.getContext("2d");
+
+//ctx2.fillStyle = 'white';
+//ctx2.fillRect(10,10, 50, 70);
+
+
+ctx.fillStyle= 'red';
+ctx.strokeStyle = 'white';
+ctx.lineWidth = 15;
+ctx.beginPath();
+ctx.arc(300, 300, 50, 0, Math.pi*2, true);
+ctx.fill();
+ctx.stroke();
+console.log(ctx);
