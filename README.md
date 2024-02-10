d90f5ebbeaa576599de47c8fb684ba358dc45bfa
crystalyn <00crystalynthompson@gmail.com>
2024-02-10 12:49:14 -0700

    nothing is working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # canvasPractice
 just going to play around and learn the basics
+
+trying to start but i cant do anything without errors. i could copy code exactly and every bit is still giving me issues. i need to do more learning before i really make and progress.
--- index.js
+++ index.js
@@ -2,17 +2,26 @@ console.log("hi");
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-//const ctx2 = canvas.getContext("2d");
+const ctx2 = canvas.getContext("2d");
 
-//ctx2.fillStyle = 'white';
-//ctx2.fillRect(10,10, 50, 70);
+ctx2.fillStyle = 'white';
+// ctx2.strokeStyle = 'red';
+// ctx2.lineWidth = '8';
+ctx2.fillRect(10,10, 7, 7);
+// ctx2.stroke();  did nothing
 
 
 ctx.fillStyle= 'red';
-ctx.strokeStyle = 'white';
+ctx.strokeStyle = 'red';
 ctx.lineWidth = 15;
 ctx.beginPath();
 ctx.arc(300, 300, 50, 0, Math.pi*2, true);
 ctx.fill();
 ctx.stroke();
 console.log(ctx);
+console.log(ctx2);
+
+const mouse ={
+    x: null,
+    y: null,
+}
