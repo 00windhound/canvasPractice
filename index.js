c539d92899a9b582e48dbcea82c45eb9422e660d
crystalyn <00crystalynthompson@gmail.com>
2024-02-10 20:45:34 -0800

    fighting to keep the circle on the screen

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ console.log("hi");
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-const ctx2 = canvas.getContext("2d");
+const mouse={
+    x: undefined,
+    y: undefined,
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x= event.x
+    mouse.y= event.y
+    console.log(event)
+    drawcircle();
+})
+
+function drawcircle(){
+    ctx.fillStyle ='white';
+    ctx.strokeStyle ='red';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.arc(mouse.x,40,5,0, Math.PI * 2);// i add the mouse x&y and doesnt wanna show
+    ctx.fill();
+    ctx.stroke();
+    console.log(ctx)
+}
+
+/*const ctx2 = canvas.getContext("2d");
 
 ctx2.fillStyle = 'white';
 // ctx2.strokeStyle = 'red';
@@ -15,7 +38,7 @@ ctx.fillStyle= 'red';
 ctx.strokeStyle = 'red';
 ctx.lineWidth = 15;
 ctx.beginPath();
-ctx.arc(300, 300, 50, 0, Math.pi*2, true);
+ctx.arc(430, 240, 50, 0, Math.PI*2, true);
 ctx.fill();
 ctx.stroke();
 console.log(ctx);
@@ -29,7 +52,7 @@ const mouse ={
 function drawcircle(){
     ctx.fillStyle= 'red';
     ctx.beginPath();
-    ctx.arc(mouse.x,mouse.y,5,0, Math.PI*2);
+    ctx.arc(300,359,5,0, Math.PI*2);
     ctx.fill();
 }
 drawcircle();
@@ -39,5 +62,5 @@ canvas.addEventListener('click', function(event){
     mouse.y = event.y;
     console.log(event);
     drawcircle();
-})
+})*/
 
