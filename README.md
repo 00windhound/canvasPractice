bab521bfd5e73d6557831847ab93545069734837
crystalyn <00crystalynthompson@gmail.com>
2024-02-12 03:51:42 -0800

    done with tutorial

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ just going to play around and learn the basics
 
 trying to start but i cant do anything without errors. i could copy code exactly and every bit is still giving me issues. i need to do more learning before i really make and progress.
 i have the circle printing and the cordinates of the mouse but i cant put the two together?
+
+its far less glitchy when it always has the lines and their size doesnt seem to matter. not exactly the design i was going for but its much cleanner.
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ canvas.addEventListener('click', function(event){
     mouse.x= event.clientX - canrect.left;
     mouse.y= event.clientY - canrect.top;
     console.log("x=",mouse.x,"y=",mouse.y)
-    for(let y=0; y<10;y++){
+    for(let y=0; y<5;y++){
     allparticles.push(new particle())
     }
    // drawcircle(); //this one works when you click
@@ -24,34 +24,35 @@ canvas.addEventListener('click', function(event){
 canvas.addEventListener('mousemove',function(event){
     mouse.x = event.clientX - canrect.left; 
     mouse.y = event.clientY - canrect.top;
-    for(let y=0; y<5;y++){
+    for(let y=0; y<1;y++){
         allparticles.push(new particle())
     }
     //drawcircle()  // they work!!!
 })
 
+function nomouse(){
+ 
+    if(allparticles.length < 10){
+        mouse.x =Math.random()*canvas.width
+        mouse.y =Math.random()*canvas.height;
+        allparticles.push(new particle());
+        handleparticles();
+    }
+}
 
 
-/*function drawcircle(){
-    ctx.fillStyle ='white';
-    ctx.strokeStyle ='red';
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.arc(mouse.x,mouse.y,5,0, Math.PI*2 );
-    ctx.fill();
-    ctx.stroke();
-    console.log(ctx)
-}*/
+
 
 class particle{
     constructor(){
         this.x = mouse.x;
         this.y = mouse.y;
-       // this.x = Math.random()*canvas.width;
+       //this.x = Math.random()*canvas.width;
        // this.y = Math.random()*canvas.height;
         this.size = Math.random()*10+1;
         this.speedx = Math.random()*3-1.5;
         this.speedy = Math.random()*3-1.5;
+        this.color = 'hsl('+hue+',100%, 50%';
     }
     update(){
         this.x += this.speedx;
@@ -61,7 +62,7 @@ class particle{
         }
     }
     draw(){
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
         ctx.fill();
@@ -82,19 +83,35 @@ function handleparticles(){
     for(let j=0; j<allparticles.length; j++){
         allparticles[j].update();
         allparticles[j].draw();
+       
+        for(let k = j; k< allparticles.length; k++){
+            const dx = allparticles[j].x - allparticles[k].x;
+            const dy = allparticles[j].y - allparticles[k].y;
+            const distance = Math.sqrt(dx * dx * dy * dy);
+            if (distance<50000){
+                ctx.beginPath();
+                ctx.strokeStyle = allparticles[j].color;
+                ctx.lineWidth = allparticles[j].size/5;
+                ctx.moveTo(allparticles[j].x, allparticles[j].y);
+                ctx.lineTo(allparticles[k].x, allparticles[k].y);
+                ctx.stroke();
+            }
+        }
         if(allparticles[j].size<= 0.3){
             allparticles.splice(j,1);
             j--;
+            nomouse();
             //console.log(allparticles)
         }
     }
 }
 
 function animate(){
-   // ctx.clearRect(0,0,canvas.width, canvas.height);
-   ctx.fillStyle = 'rgba(0,0,0,0.01';
-   ctx.fillRect(0,0,canvas.clientWidth,canvas.height);
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+  // ctx.fillStyle = 'rgba(0,0,0,0.01';
+  // ctx.fillRect(0,0,canvas.clientWidth,canvas.height);
     handleparticles();
+    hue+=2;
     requestAnimationFrame(animate);
 }
 animate();
