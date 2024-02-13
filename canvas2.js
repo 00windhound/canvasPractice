84b37e62f817e35868e2f83160071bfb40fba26e
crystalyn <00crystalynthompson@gmail.com>
2024-02-12 17:17:22 -0700

    set up 2 pages

--- canvas2.js
+++ canvas2.js
@@ -1,11 +1,14 @@
 const canvas2 = document.getElementById('mycanvas2');
 ctx2 = canvas2.getContext('2d');
-var x = undefined;
-var y = undefined;
-x=x-canrect.left;
-y=y-canrect.top;
+var canvasrect = canvas.getBoundingClientRect();
+
+
+var x = 0;
+var y = 0;
+x=x-canvasrect.left;
+y=y-canvasrect.top;
 
 ctx2.fillStyle = 'white';
 ctx2.beginPath();
-ctx2.arc(10,10,20,0, Math.PI*2);
-ctx.fill();
+ctx2.arc(x,y,20,0, Math.PI*2);
+ctx2.fill();
