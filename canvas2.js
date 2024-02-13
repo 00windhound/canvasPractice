91499cc98c0b4568c8cc0da7b2e824b4fe397125
crystalyn <00crystalynthompson@gmail.com>
2024-02-12 20:05:33 -0700

    needs bug fixing but should be the whole base

--- canvas2.js
+++ canvas2.js
@@ -1,14 +1,60 @@
-const canvas2 = document.getElementById('mycanvas2');
-ctx2 = canvas2.getContext('2d');
+const canvas = document.getElementById('mycanvas2');
+var ctx = canvas.getContext('2d');
 var canvasrect = canvas.getBoundingClientRect();
+const allcircles = [];
+
+const mouse ={
+    x: undefined,
+    y: undefined,
+}
+
+canvas.addEventListener('click',function(event){
+    mouse.x = event.clientX - canvasrect.left + window.scrollX;
+    mouse.y = event.clientY - canvasrect.top + window.scrollY;
+   // drawcircle();
+})
 
 
-var x = 0;
-var y = 0;
-x=x-canvasrect.left;
-y=y-canvasrect.top;
+class circle{
+    constructor(){
+        //this.x = mouse.x;
+        //this.y = mouse.y;
+        this.x = Math.random()*canvas.Width
+        this.y = Math.random()*canvas.height
+        this.size = Math.random()*5+1;
+        this.speedx = Math.random()*3-1.5
+        this.speedy = Math.random()*3-1.5
+       // this.color = 
+    }
+    update(){
+        this.x += this.speedx;  // this.x is returning nan
+        this.y += this.speedy;
+    }
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,20,0, Math.PI*2);
+        ctx.fill();
+    }
+}
 
-ctx2.fillStyle = 'white';
-ctx2.beginPath();
-ctx2.arc(x,y,20,0, Math.PI*2);
-ctx2.fill();
+function handleparticles (){
+    for(let j =0; j< allcircles.length; j++){
+        allcircles[j].update();
+        allcircles[j].draw();
+    }
+}
+
+function init(){
+for(let i=0; i<100; i++){
+    allcircles.push(new circle());
+}
+}
+init()
+
+function animate(){
+    ctx.clearRect(0,0, canvas.clientWidth, canvas.height);
+    handleparticles();
+    requestAnimationFrame(animate);
+}
+animate();
