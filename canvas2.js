154191170f94ce59e4d37e28e9e038c4d9b3e0f9
crystalyn <00crystalynthompson@gmail.com>
2024-02-13 17:42:18 -0800

    a lot of the numbers are attrocious

--- canvas2.js
+++ canvas2.js
@@ -11,16 +11,18 @@ const mouse ={
 canvas.addEventListener('click',function(event){
     mouse.x = event.clientX - canvasrect.left + window.scrollX;
     mouse.y = event.clientY - canvasrect.top + window.scrollY;
-   // drawcircle();
+    console.log(mouse)
+    console.log(allcircles)
+    allcircles.push(new circle());
 })
 
 
 class circle{
     constructor(){
-        //this.x = mouse.x;
-        //this.y = mouse.y;
-        this.x = Math.random()*canvas.Width
-        this.y = Math.random()*canvas.height
+        this.x = mouse.x;
+        this.y = mouse.y;
+        //this.x = Math.random()*canvas.Width //goimg from undefined to nan
+        //this.y = Math.random()*canvas.height  //canvas width is undefined
         this.size = Math.random()*5+1;
         this.speedx = Math.random()*3-1.5
         this.speedy = Math.random()*3-1.5
@@ -29,12 +31,29 @@ class circle{
     update(){
         this.x += this.speedx;  // this.x is returning nan
         this.y += this.speedy;
+        wallcolision();
+       /* if(this.size>0.2){   // shrinks the circles down to dust
+            this.size-=0.1;
+        }*/
     }
     draw(){
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(this.x,this.y,20,0, Math.PI*2);
+        ctx.arc(this.x,this.y,this.size,0, Math.PI*2);
         ctx.fill();
+    }
+}
+
+function wallcolision(){
+    for(let k=0; k<allcircles.length; k++){
+        if(allcircles[k].x < 0 || allcircles[k].x > 1500){
+            allcircles[k].speedx === -allcircles[k].speedx;
+        }
+        else return;
+        if(allcircles[k].y < 0 || allcircles[k].y > 1500){
+            allcircles[k].speedy = -allcircles[k].speedy;
+        }
+        else return;// circle is out of frame but x is 620
     }
 }
 
@@ -46,9 +65,11 @@ function handleparticles (){
 }
 
 function init(){
-for(let i=0; i<100; i++){
-    allcircles.push(new circle());
-}
+    for(let i=0; i<100; i++){
+        mouse.x = Math.random()*1500;
+        mouse.y = Math.random()*canvas.height
+        allcircles.push(new circle());
+    }
 }
 init()
 
@@ -57,4 +78,4 @@ function animate(){
     handleparticles();
     requestAnimationFrame(animate);
 }
-animate();
+animate()
