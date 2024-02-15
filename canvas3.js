e5a01b8c5d7f5ceaa02cc28decfc6b20a1503bde
crystalyn <00crystalynthompson@gmail.com>
2024-02-15 14:19:19 -0700

    idk

--- canvas3.js
+++ canvas3.js
@@ -11,7 +11,18 @@ let framex = 0;
 let framey = 0;
 let gameframe = 0;
 const staggarframe =5;
-
+const spriteanimation = [];
+const animationstates = [
+    {
+        name: 'idle',
+        frames: 7,
+    }
+    {
+        name: 'jump',
+        frames: 7,
+    }
+]
+animationstates.forEach(() => {})
 function animate(){
     ctx.clearRect(0,0, canvaswidth, canvasheight);
     //ctx.fillRect(50, 50, 70, 80);
